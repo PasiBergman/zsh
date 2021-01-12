854a5a68b3c90c901629b9aa357c57fbb8b04d6f
Pasi Bergman <pasi.bergman@iki.fi>
2021-01-12 23:21:49 +0200

    Add brave alias 'brave --disable-gpu'

--- alias.sh
+++ alias.sh
@@ -3,4 +3,5 @@ alias ls="ls --color=auto"
 alias dotfiles="$HOME/.config/zsh/script/dotfiles-status.sh"
 alias dotfiles-status="$HOME/.config/zsh/script/dotfiles-status.sh"
 alias dotfiles-pull="$HOME/.config/zsh/script/dotfiles-pull.sh"
+alias brave="brave --disable-gpu"
 
