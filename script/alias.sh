3190f561443c0fab11e287ba19b007e3cf50c5b2
Pasi Bergman <pasi.bergman@iki.fi>
2021-01-17 19:34:59 +0000

    Add cpu-info and cpu-freq scripts and aliases

--- script/alias.sh
+++ script/alias.sh
@@ -3,13 +3,16 @@ alias ls="ls --color=auto"
 alias dotfiles="$HOME/.config/zsh/script/dotfiles-status.sh"
 alias dotfiles-status="$HOME/.config/zsh/script/dotfiles-status.sh"
 alias dotfiles-pull="$HOME/.config/zsh/script/dotfiles-pull.sh"
-alias brave="brave --disable-gpu"
 alias vim="nvim"
 alias vi="nvim"
-
+alias cpu-info="$HOME/.config/zsh/script/cpu-info.sh"
 
 # Reverse aliases. Open these filetypes with nvim
 alias -s {cs,ts,js,html,htm,md,json,yml,yaml,vue}=nvim
 
 alias mem=$'ps axc -o cmd,%mem --sort=-%mem | head -15 | awk \'{printf("%-20s %5s%\\n", $1, $2)}\''
 alias cpu=$'ps axc -o cmd,%cpu --sort=-%cpu | head -15 | awk \'{printf("%-20s %5s%\\n", $1, $2)}\''
+
+
+# Xorg / i3 specific - move to separate file
+alias brave="brave --disable-gpu"
