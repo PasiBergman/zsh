2be3924054544dbb428a6fa7fc1ad6f3152bda2a
Pasi Bergman <pasi.bergman@iki.fi>
2021-01-18 22:33:22 +0200

    Add Z

--- script/alias.sh
+++ script/alias.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 alias ls="ls --color=auto"
-alias dotfiles="$HOME/.config/zsh/script/dotfiles-status.sh"
 alias dotfiles-status="$HOME/.config/zsh/script/dotfiles-status.sh"
 alias dotfiles-pull="$HOME/.config/zsh/script/dotfiles-pull.sh"
 alias brave="brave --disable-gpu"
@@ -13,3 +12,7 @@ alias -s {cs,ts,js,html,htm,md,json,yml,yaml,vue}=nvim
 
 alias mem=$'ps axc -o cmd,%mem --sort=-%mem | head -15 | awk \'{printf("%-20s %5s%\\n", $1, $2)}\''
 alias cpu=$'ps axc -o cmd,%cpu --sort=-%cpu | head -15 | awk \'{printf("%-20s %5s%\\n", $1, $2)}\''
+
+# dotfiles alias
+# https://github.com/PasiBergman/dotfiles
+alias dotfiles='git --git-dir=/home/pasi/dotfiles/ --work-tree=/home/pasi'
